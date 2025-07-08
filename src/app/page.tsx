bb75ce75af889cdbca95fadc45728b1fb3d34071
Adam Raway <adamraway@gmail.com>
2025-07-08 16:24:09 -0400

    Updated css naming conventions to follow BEM and updated page links to be internal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import "./styles/homepage.css";
+import Link from "next/link";
 
 // Toggle this line on/off as needed
 // import "./styles/debugging.css";
@@ -17,15 +18,15 @@ export default function Home() {
           Adam Raway
         </h1>
 
-        <nav className="navBar">
+        <nav className="navigation-bar">
           {/*
             The pages of my site will include my resume page, projects & experience page,
             publications page, contact me page. Any other ideas?
           */}
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Resume</a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Projects & Experience</a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Publications</a>
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Contact Me</a>
+          <Link href="/resume">Resume</Link>
+          <Link href="/projects_experience">Projects & Experience</Link>
+          <Link href="/publications">Publications</Link>
+          <Link href="/contact">Contact Me</Link>
         </nav>
       </header>
 
@@ -37,11 +38,11 @@ export default function Home() {
         {/*
           About Me
         */}
-        <section className="aboutMe">
+        <section className="about-me">
           <p>Here is some text about me!</p>
-          <img className="profilePic"
+          <img className="about-me__image"
             src="/profile-picture.jpg"
-            alt="A portrait photo of me in front of some trees."
+            alt="Portrait of Adam Raway used for personal branding."
           ></img>
         </section>
         
@@ -65,7 +66,7 @@ export default function Home() {
       */}
       <footer>
         <p>© 2025 Adam Raway. All rights reserved.</p>
-        <nav className="navBar">
+        <nav className="navigation-bar">
           <a href="https://github.com/Adam-Raway" target="_blank" rel="noopener noreferrer">GitHub</a>
           <a href="https://linkedin.com/in/adam-raway" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="mailto:adamraway@gmail.com">Email</a>
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,14 @@
+import "../styles/homepage.css";
+import Link from "next/link";
+
+// Toggle this line on/off as needed
+// import "./styles/debugging.css";
+
+
+export default function Home() {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  );
+}
